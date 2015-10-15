15979c84faa1539a6ed67ba9b8e39820701eccb5
Kat Sullivan <kmsullivan012@gmail.com>
2015-10-15 17:49:05 -0400

    chchchcanges

--- Kat-week4/sketch.js
+++ Kat-week4/sketch.js
@@ -6,24 +6,8 @@ var r = new Rune({
 });
 
 // SHARP
-r.rect(0,0,800,800).fill('hsv', 0, 0, 50);
+r.rect(0,0,800,800).fill('hsv', 0, 10, 40);
 
-// drawCircles(250, 250, 200, 9)
-// function drawCircles(x, y, radius, level) {
-//   saturation = Math.round(Rune.random(10,80))
-//   r.ellipse(x, y, radius*2, radius*2).fill('hsv', 0, saturation, 40);
-//   if (level > 1) {
-//     level = level - 1;
-//     num = Math.round(Rune.random(2, 5));
-//     console.log
-//     for(var i=0; i<num; i++) {
-//       a = Math.random() * (Math.PI * 2);
-//       nx = x + Math.cos(a) * 6.0 * level;
-//       ny = y + Math.sin(a) * 6.0 * level;
-//       drawCircles(nx, ny, radius/2, level);
-//     }
-//   }
-// }
 a = 15.5;
 radius = 90;
 var p;
@@ -32,7 +16,8 @@ for(var i=0; i<360; i++) {
   //The exponent a controls the shape of the curve
   y = r.height/2+ Math.sin( Rune.radians(i) ) * Math.pow(Math.sin(Rune.radians(i)/2), a) *radius;
   if (i==0) {
-    p = r.path(200, 300).fill('hsv', 0, 100, 55).strokeWidth(4);
+    // p = r.path(200, 300).fill('hsv', 0, 100, 55).strokeWidth(4);
+    p = r.path(200, 300).fill('hsv', 0, 70, 40).strokeWidth(4).stroke('hsv', 0, 100, 55);
   } else {
   //ellipse(x,y, 10,10);
   //point(x,y);
@@ -55,5 +40,43 @@ p4.move(700,700);
 p5.move(300,900);
 p6.move(400,850);
 p7.move(200,500);
+
+function fuzzyCircle(x,y,radius){
+  var p = r.polygon(x,y).fill('hsv', 0, 100, 75);
+  var angle = 360/30;
+  for(var i=0; i<90; i++){
+    // radius = i%2==0 ? Rune.random(radius-10,radius-5) : Rune.random(radius+5,radius+10)
+    var x = Math.cos(Rune.radians(i * angle)) * radius;
+    var y = Math.sin(Rune.radians(i * angle)) * radius;
+    p.lineTo(x,y);
+  }
+}
+
+drawCircles(250, 250, 175, 8)
+// var x = 250
+// var y = 250
+// var radius = 200
+// var level = 9
+function drawCircles(x, y, radius, level){
+  saturation = Math.round(Rune.random(10,80))
+  value = Rune.random(45,85)
+  r.ellipse(x, y, radius*.75, radius*.75).fill('hsv', 0, 100, value, .5).stroke('hsv', 0, 100, value, .5);
+  // fuzzyCircle(x,y,radius*0.75);
+  r.ellipse(x-15, y-20, radius*.75, radius*.75).fill('hsv', 0, 100, value, .5).stroke('hsv', 0, 100, value, .5);
+  r.ellipse(x-45, y-40, radius*.75, radius*.75).fill('hsv', 0, 100, value, .5).stroke('hsv', 0, 100, value, .5);
+
+  if (level > 1) {
+    level = level - 1;
+    num = Math.round(Rune.random(2, 5));
+    console.log
+    for(var i=0; i<num; i++) {
+      a = Math.random() * (Math.PI * 2);
+      nx = x + Math.cos(a) * 30.0 * level;
+      ny = y + Math.sin(a) * 30.0 * level;
+      drawCircles(nx, ny, radius/1.5, level);
+    }
+  }
+}
+
 
 r.draw();
