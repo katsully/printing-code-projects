f45b050003121e4e98a306eec91f7dc962564748
Kat Sullivan <kmsullivan012@gmail.com>
2016-01-25 13:57:19 -0500

    cleaning up week 2

--- Kat-week2/sketch.js
+++ Kat-week2/sketch.js
@@ -10,8 +10,8 @@ r.rect(0,0,800,800).fill(0);
 r.ellipse(130,130,50,50).fill(200).stroke(255);
 
 for (var a=0,aMax=(2*Math.PI),aStep=(Math.PI/10); a<aMax; a+=aStep){
-    step = randomIntFromInterval(150, 250);
-    fillValue = randomIntFromInterval(170, 220);
+    step = Rune.random(150, 250);
+    fillValue = Rune.random(170, 220);
     px1 = 130+Math.sin(a)*30;
     py1 = 130+Math.cos(a)*30;
     px2 = 130+Math.sin(a)*(30+step);
@@ -21,10 +21,5 @@ for (var a=0,aMax=(2*Math.PI),aStep=(Math.PI/10); a<aMax; a+=aStep){
 };
 
 r.ellipse(550,550,700,700).fill(200);
-
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
 
 r.draw();
